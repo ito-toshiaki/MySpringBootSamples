8e293936fd7e9fc70cf42e85c7c2f3a2aba08ecf
Toshiaki Ito <evolution@1024kb.cx>
2018-01-29 21:57:26 +0900

    init.

---
+++ src/main/java/cx/mb/myspringbootsamples/HomeController.java
@@ -0,0 +1,13 @@
+package cx.mb.myspringbootsamples;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String home() {
+        return "/index";
+    }
+}
---
+++ src/main/java/cx/mb/myspringbootsamples/MySpringBootSamplesApplication.java
@@ -0,0 +1,12 @@
+package cx.mb.myspringbootsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootSamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootSamplesApplication.class, args);
+    }
+}
